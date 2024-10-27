a0dcbd1cef5346d7c25fe563f38b2ee7699aeb31
Camilo Rojas <cm.andres022@hotmail.com>
2024-10-27 17:48:28 -0500

    Update: Fixed .net warnings and erros on project

--- SimpleWeb.FrontEnd.App/App.xaml.cs
+++ SimpleWeb.FrontEnd.App/App.xaml.cs
@@ -1,6 +1,6 @@
 ﻿namespace SimpleWeb.FrontEnd.App;
 
-public partial class App : Application
+public partial class App
 {
     public App()
     {
--- SimpleWeb.FrontEnd.App/AppShell.xaml.cs
+++ SimpleWeb.FrontEnd.App/AppShell.xaml.cs
@@ -1,6 +1,6 @@
 ﻿namespace SimpleWeb.FrontEnd.App;
 
-public partial class AppShell : Shell
+public partial class AppShell
 {
     public AppShell()
     {
